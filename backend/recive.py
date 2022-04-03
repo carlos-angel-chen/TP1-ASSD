fcf9a55e48e2dd5f36694cfef4ba890a2b5848d9
Dimas <dbosch@itba.edu.ar>
2022-04-03 16:02:44 -0300

    Empecé backend (recieve.py) y correji nombre checkbox

---
+++ backend/recive.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def Recieve(signal_type, Amplitud, frec, theta, periodo, DC, FAA_ON, SyH_ON, LLA_ON, FR_ON):
+    if signal_type == "Coseno":
+        signal = Amplitud * np.cos (2*np)
+    elif signal_type == "Seno (T*3/2)":
+        pass
+    else:
+        pass
+    pass
+
+def FAA():
+    pass
+
+def SyH():
+    pass
+
+def LLA():
+    pass
+
+def FR():
+    pass
