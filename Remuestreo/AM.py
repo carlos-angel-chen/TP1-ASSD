9f3f2f0d496dfce48b8c13324ab9a28931f395fc
Carlos <31170466+CarlosAngelChen@users.noreply.github.com>
2022-04-12 02:49:47 -0300

    simulacion AM

---
+++ Remuestreo/AM.py
@@ -0,0 +1,20 @@
+from scipy import signal 
+import matplotlib.pyplot as plt 
+import numpy as np 
+  
+t = np.linspace(0, 1e-3, 10000, endpoint=True) 
+f = 15e3
+y = 0.5*np.cos(2*np.pi*1.8*f*t) + np.cos(2*np.pi*2*f*t) + 0.5*np.cos(2*np.pi*2.2*f*t)
+
+file = open("AM.txt", "w")
+for i in range(len(t)):
+    file.write(str(t[i]) + "\t" + str(y[i]) + "\n")
+file.close()
+
+# plt.plot(t, y)   
+# plt.xlabel('Time') 
+# plt.ylabel('Amplitude') 
+# plt.title('Sawtooth Signal - Geeksforgeeks') 
+# plt.grid(True)
+  
+# plt.show()
